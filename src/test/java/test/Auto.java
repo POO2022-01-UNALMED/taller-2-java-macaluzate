56ed3d448b23502f5211269b00cc699ca41c34b3
macaluzate <101429037+macaluzate@users.noreply.github.com>
2022-03-21 20:00:36 -0500

    Create Auto.java

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,54 @@
+package test;
+
+public class Auto {
+	String modelo;
+	int precio;
+	Asiento[] asientos;
+	String marca;
+	Motor motor;
+	int registro;
+	int cantidadCreados;
+	
+	// Methods
+	
+	int cantidadAsientos() {
+		
+		int contadorasiento=0;
+		
+		for (int i=0; i< asientos.length;i++) {
+			if (asientos[i] != null) {
+				contadorasiento++;
+				
+				
+			}
+		}
+		
+		return contadorasiento;
+	}
+	
+	
+	String verificarIntegridad()
+	{
+		
+		
+		if (registro == motor.registro) 
+		{
+			for(Asiento  asiento: asientos) 
+			{
+				
+				if (asiento != null)
+				{
+					if (registro != asiento.registro)
+					{
+						return "Las piezas no son originales";
+						
+					}
+				}
+			}
+			return "Auto original";
+			
+						
+		}
+		return "Las piezas no son originales";	
+	}
+}
