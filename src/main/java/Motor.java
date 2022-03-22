1a30eabd107157d09f56c3ccc9bbce67de9643e3
Mateo Cañavera <mcanavera@unal.edu.co>
2022-03-21 22:51:10 -0500

    Test

---
+++ src/main/java/Motor.java
@@ -0,0 +1,24 @@
+package test;
+public class Motor {
+	int NumeroCilindros;
+	String tipo;
+	int registro;
+	
+	//Methods
+	
+	void CambiarRegistro(int registro) {
+		
+	
+	}
+	
+	
+	void asignarTipo(String tipo) {
+		
+		if(tipo== "electrico" || tipo=="gasolina"){
+			
+			this.tipo=tipo;
+			
+					
+		}
+}
+}
