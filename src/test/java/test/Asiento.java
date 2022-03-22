0233234f0b2e2e52af8073cc3075080214627210
macaluzate <101429037+macaluzate@users.noreply.github.com>
2022-03-21 20:00:07 -0500

    Create Asiento.java

---
+++ src/test/java/test/Asiento.java
@@ -0,0 +1,21 @@
+package test;
+public class Asiento {
+	
+	/*Attributes*/
+	
+	 String color;
+	 int precio;
+	 int registro;
+	 	
+	
+	/*Methods*/
+	 
+	 void Cambiarcolor(String color) {
+		 if (color== "rojo " || color== "verde" || color=="amarillo" || color== "negro" || color=="blanco") {
+			 
+			 this.color=color;
+		 
+		
+		 }
+		 }		 
+}
