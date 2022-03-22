32ff520cd6c098479faff0c2034328e00029b7cd
Mateo Cañavera <mcanavera@unal.edu.co>
2022-03-21 23:53:02 -0500

    Update Asiento.java

--- src/test/java/test/Asiento.java
+++ src/test/java/test/Asiento.java
@@ -1,13 +1,15 @@
 package test;
 public class Asiento {
 	
+
+	
 	 String color;
 	 int precio;
 	 int registro;
 	 
 	 
-	 void cambiarcolor(String color) {
-		 if (color==("rojo")|| color==("verde") || color==("amarillo") || color==("negro") || color==("blanco")) {
+	 void cambiarColor(String color) {
+		 if (color.equals("rojo")|| color.equals("verde") || color.equals("amarillo") || color.equals("negro") || color.equals("blanco")) {
 			 
 			 this.color=color;
 		 
